24e775a7a1f2414c2bfbd0d8703266261654f4ca
SmileTTP <ibtissamlazraf@gmail.com>
2025-10-19 20:38:45 +0100

    2 new interfaces

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -28,4 +28,14 @@ export interface UserProps {
     };
 }
 
+export interface PostData {
+    userId: number;
+    id?: number;
+    title: string;
+    body: string;
+}
 
+export interface PostModalProps {
+    onClose: () => void;
+    onSubmit: (post: PostData) => void;
+}
