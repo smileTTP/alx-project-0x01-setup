19e19fa7e37aeaa7310ad9befc33ba21fd43e06f
SmileTTP <ibtissamlazraf@gmail.com>
2025-10-19 18:52:07 +0100

    Set up navigation between pages

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,1 +1,11 @@
+import Header from "@/components/layout/Header";
 
+const Users: React.FC = () => {
+    return (
+        <div className="flex flex-col h-screen">
+            <Header />
+        </div>
+    )
+}
+
+export default Users;
